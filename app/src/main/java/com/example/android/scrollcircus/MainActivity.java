1f208c5a7f58727f743a7911d661c7125e2b35af
MariaViolaki <mariaviolaki@outlook.com>
2017-12-23 15:01:42 +0200

    Categorize and clarify resources

--- app/src/main/java/com/example/android/scrollcircus/MainActivity.java
+++ app/src/main/java/com/example/android/scrollcircus/MainActivity.java
@@ -25,145 +25,176 @@ public class MainActivity extends AppCompatActivity {
 
     public void randomizePoints(View view) {
         if (buttonClicked == 0) {
-            buttonClicked ++;
+            buttonClicked++;
             // Generate random number using multiples of 5
-            points = ((int)(Math.random() * 15 + 2) * 5);
+            points = ((int) (Math.random() * 15 + 2) * 5);
             // Show the number on the screen
             TextView numberOfPoints = findViewById(R.id.number_of_points);
             numberOfPoints.setText("" + points);
-        }
-        else {
-            buttonClicked ++;
+        } else {
+            buttonClicked++;
             Toast.makeText(this, "You can't change your fate.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void resourceOne(View view) {
+    public void addBudget(View view) {
         // Find Switch on XML
-        Switch switchOne = findViewById(R.id.switch_one);
+        Switch budget = findViewById(R.id.budget);
         // Allocate points to Switch
-        if (switchOne.isChecked()) {
-            if (points >= 15) {
-                points = points - 15;
+        if (budget.isChecked()) {
+            if (points >= 25) {
+                points = points - 25;
             } else {
-                switchOne.setChecked(false);
+                budget.setChecked(false);
             }
         } else {
-            points = points + 15;
-            switchOne.setChecked(false);
+            points = points + 25;
+            budget.setChecked(false);
         }
         // Show new resources number
         TextView numberOfPoints = findViewById(R.id.number_of_points);
         numberOfPoints.setText("" + points);
     }
 
-    public void resourceTwo(View view) {
-        Switch switchTwo = findViewById(R.id.switch_two);
-        if (switchTwo.isChecked()) {
-            if (points >= 10) {
-                points = points - 10;
-            } else {
-                switchTwo.setChecked(false);
-            }
-        } else {
-            points = points + 10;
-            switchTwo.setChecked(false);
-        }
-        TextView numberOfPoints = findViewById(R.id.number_of_points);
-        numberOfPoints.setText("" + points);
-    }
-
-    public void resourceThree(View view) {
-        Switch switchThree = findViewById(R.id.switch_three);
-        if (switchThree.isChecked()) {
+    public void addKnowledge(View view) {
+        Switch knowledge = findViewById(R.id.knowledge);
+        if (knowledge.isChecked()) {
             if (points >= 20) {
                 points = points - 20;
             } else {
-                switchThree.setChecked(false);
+                knowledge.setChecked(false);
             }
         } else {
             points = points + 20;
-            switchThree.setChecked(false);
+            knowledge.setChecked(false);
         }
         TextView numberOfPoints = findViewById(R.id.number_of_points);
         numberOfPoints.setText("" + points);
     }
 
-    public void resourceFour(View view) {
-        Switch switchFour = findViewById(R.id.switch_four);
-        if (switchFour.isChecked()) {
-            if (points >= 5) {
-                points = points - 5;
-            } else {
-                switchFour.setChecked(false);
-            }
-        } else {
-            points = points + 5;
-            switchFour.setChecked(false);
-        }
-        TextView numberOfPoints = findViewById(R.id.number_of_points);
-        numberOfPoints.setText("" + points);
-    }
-
-    public void resourceFive(View view) {
-        Switch switchFive = findViewById(R.id.switch_five);
-        if (switchFive.isChecked()) {
-            if (points >= 25) {
-                points = points - 25;
-            } else {
-                switchFive.setChecked(false);
-            }
-        } else {
-            points = points + 25;
-            switchFive.setChecked(false);
-        }
-        TextView numberOfPoints = findViewById(R.id.number_of_points);
-        numberOfPoints.setText("" + points);
-    }
-
-    public void resourceSix(View view) {
-        Switch switchSix = findViewById(R.id.switch_six);
-        if (switchSix.isChecked()) {
-            if (points >= 10) {
-                points = points - 10;
-            } else {
-                switchSix.setChecked(false);
-            }
-        } else {
-            points = points + 10;
-            switchSix.setChecked(false);
-        }
-        TextView numberOfPoints = findViewById(R.id.number_of_points);
-        numberOfPoints.setText("" + points);
-    }
-
-    public void resourceSeven(View view) {
-        Switch switchSeven = findViewById(R.id.switch_seven);
-        if (switchSeven.isChecked()) {
+    public void addEntertainment(View view) {
+        Switch entertainment = findViewById(R.id.entertainment);
+        if (entertainment.isChecked()) {
             if (points >= 20) {
                 points = points - 20;
             } else {
-                switchSeven.setChecked(false);
+                entertainment.setChecked(false);
             }
         } else {
             points = points + 20;
-            switchSeven.setChecked(false);
+            entertainment.setChecked(false);
         }
         TextView numberOfPoints = findViewById(R.id.number_of_points);
         numberOfPoints.setText("" + points);
     }
 
-    public void resourceEight(View view) {
-        Switch switchEight = findViewById(R.id.switch_eight);
-        if (switchEight.isChecked()) {
+    public void addCaution(View view) {
+        Switch caution = findViewById(R.id.caution);
+        if (caution.isChecked()) {
             if (points >= 15) {
                 points = points - 15;
             } else {
-                switchEight.setChecked(false);
+                caution.setChecked(false);
             }
         } else {
             points = points + 15;
-            switchEight.setChecked(false);
+            caution.setChecked(false);
+        }
+        TextView numberOfPoints = findViewById(R.id.number_of_points);
+        numberOfPoints.setText("" + points);
+    }
+
+    public void addStrength(View view) {
+        Switch strength = findViewById(R.id.strength);
+        if (strength.isChecked()) {
+            if (points >= 15) {
+                points = points - 15;
+            } else {
+                strength.setChecked(false);
+            }
+        } else {
+            points = points + 15;
+            strength.setChecked(false);
+        }
+        TextView numberOfPoints = findViewById(R.id.number_of_points);
+        numberOfPoints.setText("" + points);
+    }
+
+    public void addFlexibility(View view) {
+        Switch flexibility = findViewById(R.id.flexibility);
+        if (flexibility.isChecked()) {
+            if (points >= 10) {
+                points = points - 10;
+            } else {
+                flexibility.setChecked(false);
+            }
+        } else {
+            points = points + 10;
+            flexibility.setChecked(false);
+        }
+        TextView numberOfPoints = findViewById(R.id.number_of_points);
+        numberOfPoints.setText("" + points);
+    }
+
+    public void addSnacks(View view) {
+        Switch snacks = findViewById(R.id.snacks);
+        if (snacks.isChecked()) {
+            if (points >= 10) {
+                points = points - 10;
+            } else {
+                snacks.setChecked(false);
+            }
+        } else {
+            points = points + 10;
+            snacks.setChecked(false);
+        }
+        TextView numberOfPoints = findViewById(R.id.number_of_points);
+        numberOfPoints.setText("" + points);
+    }
+
+    public void addMerchandising(View view) {
+        Switch merchandising = findViewById(R.id.merchandising);
+        if (merchandising.isChecked()) {
+            if (points >= 10) {
+                points = points - 10;
+            } else {
+                merchandising.setChecked(false);
+            }
+        } else {
+            points = points + 10;
+            merchandising.setChecked(false);
+        }
+        TextView numberOfPoints = findViewById(R.id.number_of_points);
+        numberOfPoints.setText("" + points);
+    }
+
+    public void addAnimals(View view) {
+        Switch animals = findViewById(R.id.animals);
+        if (animals.isChecked()) {
+            if (points >= 5) {
+                points = points - 5;
+            } else {
+                animals.setChecked(false);
+            }
+        } else {
+            points = points + 5;
+            animals.setChecked(false);
+        }
+        TextView numberOfPoints = findViewById(R.id.number_of_points);
+        numberOfPoints.setText("" + points);
+    }
+
+    public void addFreaks(View view) {
+        Switch freaks = findViewById(R.id.freaks);
+        if (freaks.isChecked()) {
+            if (points >= 5) {
+                points = points - 5;
+            } else {
+                freaks.setChecked(false);
+            }
+        } else {
+            points = points + 5;
+            freaks.setChecked(false);
         }
         TextView numberOfPoints = findViewById(R.id.number_of_points);
         numberOfPoints.setText("" + points);
